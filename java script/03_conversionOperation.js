9cf63424ec99874ddf16624936af0ae3e8632b3f
Mannashouryadeep78 <114353627+Mannashouryadeep78@users.noreply.github.com>
2023-12-26 23:37:49 +0530

    Add files via upload

---
+++ java script/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "4"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1=> true; 0 => false
+// "" => false
+//"hitesh" => true
+ let stringg = 33
+ let isstring = String(stringg)
+ console.log(isstring);
+ console.log(typeof(isstring));
